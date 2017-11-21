3fc35a4f716bb3abf2ab89c2d9ecd4e821808b85
Tri Nguyen <trint.it@gmail.com>
2017-11-21 14:05:55 -0600

    latest code solved

--- src/main/java/mum/cs544/project/entity/Person.java
+++ src/main/java/mum/cs544/project/entity/Person.java
@@ -66,36 +66,18 @@ public class Person {
 		this.lastName = lastName;
 	}
 	
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 92aeb3a3ef5239627fbd734a49da3bfe2f1f0ca0
 	public String getEmail() {
 		return email;
 	}
 	public void setEmail(String email) {
 		this.email = email;
 	}
-<<<<<<< HEAD
 	public List<Appointment> getAppointments() {
 		return appointments;
 	}
 	public void setAppointments(List<Appointment> appointments) {
 		this.appointments = appointments;
 	}
-=======
-	public List<Appointment> getAppointments() {
-		return appointments;
-	}
-	public void setAppointments(List<Appointment> appointments) {
-		this.appointments = appointments;
-	}
-=======
-	public List<Appointment> getAppointments() {
-		return appointments;
-	}
->>>>>>> fdfcae4a14f8a48cfc7c0fed2a413f1b70ae29d0
->>>>>>> 92aeb3a3ef5239627fbd734a49da3bfe2f1f0ca0
 	public String getFullName() {
 		fullName = firstName + " " + lastName;
 		return fullName;
