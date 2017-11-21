6dbf25d244ef90330b36c0634e7470d41c1b4194
amaraa0327 <amaraa0327@gmail.com>
2017-11-21 16:53:41 -0600

    creat new and list appointment

--- src/main/java/mum/cs544/project/service/IAppointmentService.java
+++ src/main/java/mum/cs544/project/service/IAppointmentService.java
@@ -7,7 +7,7 @@ import mum.cs544.project.entity.Person;
 
 public interface IAppointmentService {
 	public boolean createAppointment(Appointment appt);
-	public boolean deleteAppointment(Appointment appt);
+	public boolean deleteAppointment(long id);
 	public List<Appointment> getAppointmentsOfCustomer(Person person);
 	public List<Appointment> getAppointmentsByReminder(int reminder);
 	public void save(Appointment appointment);
